ecbfafaeda697ca8186ccd0bfb3c75354f783544
jiwoong264 <jiwoong264@gmail.com>
2024-03-18 19:50:25 +0900

    [ADD] Add essets, create LoginVC.swift

---
+++ Source/Presenter/Login/LoginVC.swift
@@ -0,0 +1,35 @@
+//
+//  LoginVC.swift
+//  iOS_Nuflect
+//
+//  Created by Jiwoong's MacBook Air on 3/18/24.
+//
+
+import UIKit
+import SnapKit
+
+class LoginVC: UIViewController {
+    //MARK: - UI ProPerties
+    lazy var navigationBar = UINavigationBar()
+
+    //MARK: - Define Method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SetView()
+        Constraint()
+    }
+    
+    //MARK: - Properties
+    
+    
+    //MARK: - Set Ui
+    func SetView() {
+        
+    }
+    
+    func Constraint() {
+        
+    }
+  
+
+}
